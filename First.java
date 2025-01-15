8cba21e8b818d1b247f8bcc6767e9af13858f3ed
vickykrsingh27 <vickykrsingh27@gmail.com>
2025-01-15 18:47:52 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.out.println(num1);
+        sc.close();
+    }
+}
