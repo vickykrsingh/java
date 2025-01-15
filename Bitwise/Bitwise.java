5081ce8b5b8dffcac440a1ed4012741184e7cd24
vickykrsingh27 <vickykrsingh27@gmail.com>
2025-01-15 19:07:07 +0530

    Bitwise

---
+++ Bitwise/Bitwise.java
@@ -0,0 +1,12 @@
+package Bitwise;
+
+public class Bitwise {
+    public static void main(String[] args) {
+        System.out.println(9|10); //11
+        System.out.println(9&10); // 8
+        System.out.println(9^10); // 3
+        System.out.println(~9); // 
+        System.out.println(9<<1); // 9*2^1 -> 18
+        System.out.println(9<<4); // 9*2^4 -> 9*16 -> 144
+    }
+}
